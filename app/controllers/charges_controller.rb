ffefb3ed4309f7bca3ba00b72ed7770b8f89344a
NathanSalugsugan <nathan.ess@gmail.com>
2015-07-08 15:21:55 +0800

    Implemented required I want this button on the purchase page

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,14 +7,14 @@ class ChargesController < ApplicationController
   	customer = Stripe::Customer.create(
       :email => 'example@stripe.com',
       :card  => params[:stripeToken]
-  		)
+  	)
 
   	charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @amount,
       :description => 'Rails Stripe customer',
       :currency    => 'usd'
-  	   )
+  	)
 
  	rescue Stripe::CardError => e
   	  flash[:error] = e.message
